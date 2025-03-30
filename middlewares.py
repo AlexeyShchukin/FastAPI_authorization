7d48952153e54e434b597e17d4feab3f91bd0c1b
AlexeyShchukin <shchukin.developer@gmail.com>
2025-03-30 13:05:28 +0200

    added CORS

---
+++ middlewares.py
@@ -0,0 +1,11 @@
+from starlette.middleware.cors import CORSMiddleware
+
+from main import app
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:8000",
+        "http://localhost:8000"
+    ],
+)
